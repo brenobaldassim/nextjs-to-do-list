d49acb94ec5c8b2ae4f20efa8c93c6700919725c
Breno Baldassim <brenobaldassim.contato@gmail.com>
2025-10-03 09:11:50 -0300

    feat: ssr again on home

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -1,28 +1,36 @@
 import Link from "next/link";
 import { Plus } from "lucide-react";
 import { TarefaList } from "./components/TarefaList";
+import { trpc, HydrateClient } from "@/server/trpc/server";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-const Home: React.FC = () => {
+const Home: React.FC = async () => {
+  // prefetch initial data on the server for infinite scroll
+  await trpc.tarefa.infinite.prefetchInfinite({
+    limit: 6,
+  });
+
   return (
-    <div className="font-sans min-h-screen p-8 bg-dark-primary">
-      <div className="max-w-2xl mx-auto">
-        <div className="flex justify-between items-center mb-6">
-          <h1 className="text-3xl font-bold text-light-primary">
-            Lista de Tarefas
-          </h1>
-          <Link
-            href="/tarefa"
-            className="bg-dark-secondary text-light-primary px-4 py-2 rounded-lg hover:bg-light-secondary hover:text-dark-primary font-medium flex items-center gap-2 transition-colors"
-          >
-            <Plus size={16} /> Nova Tarefa
-          </Link>
+    <HydrateClient>
+      <div className="font-sans min-h-screen p-8 bg-dark-primary">
+        <div className="max-w-2xl mx-auto">
+          <div className="flex justify-between items-center mb-6">
+            <h1 className="text-3xl font-bold text-light-primary">
+              Lista de Tarefas
+            </h1>
+            <Link
+              href="/tarefa"
+              className="bg-dark-secondary text-light-primary px-4 py-2 rounded-lg hover:bg-light-secondary hover:text-dark-primary font-medium flex items-center gap-2 transition-colors"
+            >
+              <Plus size={16} /> Nova Tarefa
+            </Link>
+          </div>
+          <TarefaList />
         </div>
-        <TarefaList />
       </div>
-    </div>
+    </HydrateClient>
   );
 };
 
